69f999c8818b40f9dbf2ac1269c334196073860f
Andronique <andronique64@gmail.com>
2025-07-09 06:40:09 +0300

    Finalisation du projet

---
+++ algodantzig/src/app/page.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react"; // Importing the arrow icon from Lucide React
+
+export default function Home() {
+  return (
+    <div
+      className="min-h-screen font-[family-name:var(--font-geist-sans)]" // Beige background color (adjust shade as needed)
+    >
+      <div className="flex items-center flex-col justify-center h-screen text-center">
+        <h1
+          className="font-[family-name:var(--font-geist-sans)] px-4 py-2 text-4xl font-extrabold" >
+          Algorithme de Dantzig :<br /> Maximisation et Minimisation
+        </h1>
+        <Link href="/home">
+          <Button className="cursor-pointer">
+            Commençons maintenant <ArrowRight className="ml-2 inline-block" size={18} />
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
+}
---
+++ algodantzig/src/components/Addnoeud.tsx
@@ -0,0 +1,47 @@
+'use client'
+import { useCallback } from 'react';
+import ReactFlow,{  Node ,addEdge, Background , Controls, useNodesState , useEdgesState, Connection } from 'reactflow';
+import 'reactflow/dist/style.css';
+
+export default function Addnoeud() {
+  return (
+    <div>Addnoeud</div>
+  )
+}
+
+
+// const initialNodes: Node[] = [
+//   {
+//     id: "1", data:{ label: "node 1"}, position: {x:0, y:0},
+//   },
+//   {
+//     id: "2", data:{label: "Node 2"}, position: {x:200, y:200},
+//   },
+//   {
+//     id: "3", data:{label: "Node 3"}, position: {x:200, y:300},
+//   },
+// ];
+
+// const initialEdges = [
+//   { id: "e1-2", source: "1", target: "2", animated: true}];
+// export default function Graphalgo() {
+//   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+//   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+//   const onConnect = useCallback((connection: Connection) => {
+//     const edge = { ...connection, animated: true , id: `${edges.length + 1}`};
+//     setEdges((preveEdges) => addEdge(edge, preveEdges));
+//   }, []);
+
+//     return (
+//       <div className=' flex-1 h-full'>
+//         <ReactFlow nodes={nodes} edges={edges}   
+//         onNodesChange={onNodesChange} 
+//         onEdgesChange={onEdgesChange} 
+//         onConnect={onConnect} fitView > 
+//           <Background />
+//           <Controls />
+//         </ReactFlow>
+//       </div>
+//     );
+//   }
+  
